556fa10ff9cb8b21cef5781e9a79a042b3d1aa04
param <paramon.j.e.k@gmail.com>
2020-11-22 21:59:47 +0300

    3 hw tasks 1, 2 with tests

---
+++ homework_04/task_2_mock_input.py
@@ -0,0 +1,41 @@
+"""
+Write a function that accepts an URL as input
+and count how many letters `i` are present in the HTML by this URL.
+
+Write a test that check that your function works.
+Test should use Mock instead of real network interactions.
+
+You can use urlopen* or any other network libraries.
+In case of any network error raise ValueError("Unreachable {url}).
+
+Definition of done:
+ - function is created
+ - function is properly formatted
+ - function has positive and negative tests
+
+You will learn:
+ - how to test using mocks
+ - how to write complex mocks
+ - how to raise an exception form mocks
+ - do a simple network requests
+
+
+>> count_dots_on_i("https://example.com/")
+59
+
+* https://docs.python.org/3/library/urllib.request.html#urllib.request.urlopen
+"""
+import http.client
+import urllib.request
+import urllib.response
+from urllib.error import HTTPError
+
+
+def count_dots_on_i(url: str) -> int:
+    try:
+        page = urllib.request.urlopen(url)
+        print(page)
+    except HTTPError as e:
+        raise Exception(f"Unreachable {url}", e)
+    result = sum(sum(1 for char in line.decode() if char == "i") for line in page)
+    return result
---
+++ homework_04/tests/test_task_2_mock_input.py
@@ -0,0 +1,32 @@
+import urllib.request
+from unittest.mock import Mock
+from urllib.error import HTTPError
+
+import pytest
+
+from homework_04.task_2_mock_input import count_dots_on_i
+
+
+def test_without_mock():
+    actual_result = count_dots_on_i("https://example.com/")
+    assert actual_result == 59
+
+
+def test_positive_with_mock(monkeypatch):
+    mock = Mock
+    fake_page = mock(
+        return_value=[b"<html><head>head</head><text>simple iximple</text></html>"]
+    )
+    monkeypatch.setattr(urllib.request, "urlopen", value=fake_page)
+
+    actual_result = count_dots_on_i("https://example.com/")
+    assert actual_result == 3
+
+
+def test_negative_bad_connection(monkeypatch):
+    url = "https://example.com/"
+    mock = Mock
+    response = mock(side_effect=HTTPError(404, "NF", "", "", ""))
+    monkeypatch.setattr(urllib.request, "urlopen", value=response)
+    with pytest.raises(Exception, match=f"Unreachable {url}"):
+        count_dots_on_i(url)

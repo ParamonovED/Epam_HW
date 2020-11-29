85ff6c1375fe1585b7780fb58a05fc81250a56cb
param <paramon.j.e.k@gmail.com>
2020-11-29 23:33:42 +0300

    hw_03: fixed 4 task

--- homework_04/task_4_doctest.py
+++ homework_04/task_4_doctest.py
@@ -24,21 +24,6 @@ You will learn:
  - how to run doctests
  - how to write instructions
 
-
->>> fizzbuzz(5)
-['1', '2', 'fizz', '4', 'buzz']
-
->>> fizzbuzz(15)
-['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizzbuzz']
-
->>> fizzbuzz(-1)
-Traceback (most recent call last):
-...
-ValueError: Input is not positive!
-
->>> fizzbuzz(0)
-[]
-
 * https://en.wikipedia.org/wiki/Fizz_buzz
 ** Энциклопедия профессора Фортрана page 14, 15, "Робот Фортран, чисть картошку!"
 """
@@ -46,19 +31,57 @@ from typing import List
 
 
 def fizzbuzz(n: int) -> List[str]:
+    """
+    Here main fizzbuzz function, but it won't work itself!
+    >>> fizzbuzz(5)
+    ['1', '2', 'fizz', '4', 'buzz']
+
+    >>> fizzbuzz(15)
+    ['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizzbuzz']
+
+    >>> fizzbuzz(-1)
+    Traceback (most recent call last):
+    ...
+    ValueError: Input is not positive!
+
+    >>> fizzbuzz(0)
+    []
+
+    """
     check_input(n)
     return [calculate_fizzbuzz(n) for n in range(1, n + 1)]
 
 
 def calculate_fizzbuzz(n):
+    """
+    Here we calculate numbers. We can make some changes easily by this way
+
+    >>> calculate_fizzbuzz(0)
+    '0'
+
+    >>> calculate_fizzbuzz(1)
+    '1'
+
+    >>> calculate_fizzbuzz(3)
+    'fizz'
+
+    >>> calculate_fizzbuzz(5)
+    'buzz'
+
+    >>> calculate_fizzbuzz(15)
+    'fizzbuzz'
+    """
     dividers = {15: "fizzbuzz", 3: "fizz", 5: "buzz"}
     for k, v in dividers.items():
-        if n % int(k) == 0:
+        if n % int(k) == 0 and n > 0:
             return v
     return str(n)
 
 
 def check_input(n):
+    """
+    That function checks is input is correct or not, we can't get get negative amount of values
+    """
     if n < 0:
         raise ValueError("Input is not positive!")
 

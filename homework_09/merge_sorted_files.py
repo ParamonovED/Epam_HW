5d2b5e85609aa695a7c1b8115321f8241ca579aa
param <paramon.j.e.k@gmail.com>
2021-01-31 19:42:06 +0300

    hw 09: changed sort function

--- homework_09/merge_sorted_files.py
+++ homework_09/merge_sorted_files.py
@@ -25,41 +25,4 @@ def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
             for line in fi:
                 big_massive.append(int(line.strip()))
 
-    return (i for i in merge_sort(big_massive))  # maybe sorted(big_massive)
-
-
-def merge_sort(data_list: List) -> List:
-    if len(data_list) < 2:
-        return data_list
-
-    middle = len(data_list) // 2
-    half_1 = data_list[:middle]
-    half_2 = data_list[middle:]
-
-    range1 = merge_sort(half_1)
-    range2 = merge_sort(half_2)
-
-    index_1 = index_2 = index_result = 0
-    result = [0] * len(data_list)
-
-    while index_1 < len(range1) and index_2 < len(range2):
-        if range1[index_1] <= range2[index_2]:
-            result[index_result] = range1[index_1]
-            index_1 += 1
-            index_result += 1
-        else:
-            result[index_result] = range2[index_2]
-            index_2 += 1
-            index_result += 1
-
-    while index_1 < len(range1):
-        result[index_result] = range1[index_1]
-        index_1 += 1
-        index_result += 1
-
-    while index_2 < len(range2):
-        result[index_result] = range2[index_2]
-        index_2 += 1
-        index_result += 1
-
-    return result
+    return (i for i in sorted(big_massive))
